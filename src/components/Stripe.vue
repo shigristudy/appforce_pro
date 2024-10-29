ec212fab99114b856525883d39f207a345787443
Kabeer <shigristudy@gmail.com>
2024-10-29 15:29:16 +0500

    Changes

--- src/components/Stripe.vue
+++ src/components/Stripe.vue
@@ -1,13 +1,14 @@
 <template>
-  <form id="payment-form" class="max-w-xl m-auto flex flex-col justify-center items-center border border-gray-300 p-2 rounded-md my-2">
+  <form id="payment-form"
+    class="max-w-xl m-auto flex flex-col justify-center items-center border border-gray-300 p-2 rounded-md my-2">
     <div id="payment-element" class=" w-full">
       <!-- Stripe will create form elements here -->
     </div>
-    <button 
-      class="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600 self-start"
-      type="submit" @click="handleSubmit">Pay via Stripe</button>
+    <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600 self-start" type="submit"
+      @click="handleSubmit">Pay via Stripe</button>
 
-    <div v-if="message" class="mt-2 text-center text-sm text-white p-2" :class="message.includes('Successful') ? 'bg-green-500' : 'bg-red-500'">
+    <div v-if="message" class="mt-2 text-center text-sm text-white p-2 font-bold"
+      :class="message.includes('Successful') ? 'text-green-500' : 'text-red-500'">
       {{ message }}
     </div>
 
@@ -29,14 +30,14 @@ const STRIPE_PUBLISHABLE_KEY = "pk_test_51NSd7OIEpzgpRspxl8geRr48D6D4r0Ol9I7ay8d
 const INITIATE_PAYMENT_API = `${BASE_URL}/payment/initiate`;
 const PAYMENT_SUCCESS_API = `${BASE_URL}/payment/complete`;
 const PAYMENT_FAILURE_API = `${BASE_URL}/payment/failure`;
-
+const PAYMENT_INTENT_ID = ref(null);
 const setupStripe = () => {
   axios.post(INITIATE_PAYMENT_API, {
     amount: 150,
     currency: 'USD'
   }).then(response => {
     const res = response.data;
-    
+    PAYMENT_INTENT_ID.value = res.payment_intent_id;
     token.value = res.token // Use to identify the payment
     stripe.value = Stripe(STRIPE_PUBLISHABLE_KEY);
     const options = {
@@ -60,6 +61,18 @@ const handleSubmit = async (e) => {
     elements: elements.value,
     redirect: "if_required"
   });
+
+  if (error === undefined) {
+    axios.post(PAYMENT_SUCCESS_API, {
+      payment_intent_id: PAYMENT_INTENT_ID.value
+    })
+  } else {
+    axios.post(PAYMENT_FAILURE_API, {
+      payment_intent_id: PAYMENT_INTENT_ID.value,
+      code: error.code,
+      description: error.message,
+    })
+  }
 
 
   // Show Message of Payment Success or Failure
